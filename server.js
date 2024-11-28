899ed2df2e7fd80e3f41c4485fbb97fb9a6c72d5
Kavya-Thota-teksacademy <kavya.t@teksacademy.com>
2024-11-28 14:25:51 +0530

    added

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ sequelize.sync().then(() => {
   console.error("Error syncing the database:", error);
 });
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
